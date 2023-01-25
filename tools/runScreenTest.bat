ea9c85fb0e1216b2cdb00c753f2affdbbcc5fc5d
BenHillel <benhillel04@gmail.com>
2023-01-25 20:17:32 +0200

    fixed - efficient drawLIne

---
+++ tools/runScreenTest.bat
@@ -0,0 +1,6 @@
+
+@echo off
+START /B JackCompiler.bat C:\Users\TLP-256\Documents\GitHub\NAND2TETRIS\projects\12 
+TIMEOUT 1
+START /B copy /Y C:\Users\TLP-256\Documents\GitHub\NAND2TETRIS\projects\12\Screen.vm C:\Users\TLP-256\Documents\GitHub\NAND2TETRIS\projects\12\ScreenTest 
+START exit 
