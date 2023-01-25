a744ea9726e9d07a84f5f51ed100d80db594d7cf
BenHillel <benhillel04@gmail.com>
2023-01-25 20:21:56 +0200

    minor bug fix

--- tools/runScreenTest.bat
+++ tools/runScreenTest.bat
@@ -2,5 +2,5 @@
 @echo off
 START /B JackCompiler.bat C:\Users\TLP-256\Documents\GitHub\NAND2TETRIS\projects\12 
 TIMEOUT 1
-START /B copy /Y C:\Users\TLP-256\Documents\GitHub\NAND2TETRIS\projects\12\Screen.vm C:\Users\TLP-256\Documents\GitHub\NAND2TETRIS\projects\12\ScreenTest 
+START /B copy /Y C:\Users\TLP-256\Documents\GitHub\NAND2TETRIS\projects\12\Screen.vm C:\Users\TLP-256\Documents\GitHub\NAND2TETRIS\projects\11\Pong
 START exit 
